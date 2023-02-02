a2ab69e475a89a6c24dd1b76e9f61ee032944f5a
Alexandre <lagarrueal@cy-tech.fr>
2023-02-02 14:11:47 +0100

    Trying to implement audio recording

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-import plotly.graph_objs as go
-import plotly.express as px
 import librosa
-#from audio_recorder_streamlit import audio_recorder
 import os
+
+import streamlit               as st
+import streamlit.components.v1 as components
+import numpy                   as np
+import pandas                  as pd
+import tensorflow              as tf
+import plotly.graph_objs       as go
+import plotly.express          as px
+
+from io                   import BytesIO
+from st_custom_components import st_audiorec
 
 def decode_audio(audio_binary):
     # Decode WAV-encoded audio files to `float32` tensors, 
@@ -74,46 +78,76 @@ option = st.selectbox(
     'Please select an audio file to try the model:',
     options)
 
-if option != "Select a file":
-    st.write('You selected:', option)
-    
-    button = st.button('Try the model')
+container_test = st.container()
+container_live = st.container()
 
-    if button:
-        st.write("Listen to the audio file")
-        st.audio("data/" + option, format='audio/wav')
-        audio = decode_audio(tf.io.read_file("data/" + option))
-        spectrogram = get_spectrogram(audio).numpy()
-        audio = audio.numpy()
-        if len(spectrogram.shape) > 2:
-            assert len(spectrogram.shape) == 3
-            spectrogram = np.squeeze(spectrogram, axis=-1)
-        log_spec = np.log(spectrogram.T + np.finfo(float).eps)
-        height = log_spec.shape[0]
-        width = log_spec.shape[1]
-        X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
-        Y = [i for i in range(height)]
+with container_test:
+    if option != "Select a file":
+        st.write('You selected:', option)
         
-        df_wf = pd.DataFrame(columns=["time","Amplitude"])
-        df_wf["time"] = np.arange(0, len(audio))
-        df_wf["Amplitude"] = audio
-        fig = px.line(df_wf, x="time", y="Amplitude", title="Audio waveform", width=800, height=400)
-        st.plotly_chart(fig)
-        
-        # Create a trace for the spectrogram
-        trace = go.Heatmap( z=log_spec, x=X, y=Y, colorscale='Viridis', showscale=False)
-        data=[trace]
-        layout = go.Layout( title="Spectrogram", width=800, height=400) 
-        fig = go.Figure(data=data, layout=layout)
-        st.plotly_chart(fig)
-        
-        tensor = tf.convert_to_tensor(spectrogram)
-        tensor = tf.expand_dims(tensor, 0)
-        tensor = np.array(tensor.numpy())
-        
-        prediction = MODEL.predict(tensor)
-        
-        # Bar chart of the prediction for each class
-        fig = go.Figure(data=[go.Bar(x=TARGETS, y=tf.nn.softmax(prediction[0]))])
-        fig.update_layout(title_text='Prediction for each class', width=800, height=400)
-        st.plotly_chart(fig)
+        button = st.button('Try the model')
+        if not button:
+            st.markdown('##')
+            st.markdown('##')
+        if button:
+            st.write("Listen to the audio file")
+            st.audio("data/" + option, format='audio/wav')
+            audio = decode_audio(tf.io.read_file("data/" + option))
+            spectrogram = get_spectrogram(audio).numpy()
+            audio = audio.numpy()
+            if len(spectrogram.shape) > 2:
+                assert len(spectrogram.shape) == 3
+                spectrogram = np.squeeze(spectrogram, axis=-1)
+            log_spec = np.log(spectrogram.T + np.finfo(float).eps)
+            height = log_spec.shape[0]
+            width = log_spec.shape[1]
+            X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
+            Y = [i for i in range(height)]
+            
+            df_wf = pd.DataFrame(columns=["time","Amplitude"])
+            df_wf["time"] = np.arange(0, len(audio))
+            df_wf["Amplitude"] = audio
+            fig = px.line(df_wf, x="time", y="Amplitude", title="Audio waveform", width=800, height=400)
+            st.plotly_chart(fig)
+            
+            # Create a trace for the spectrogram
+            trace = go.Heatmap( z=log_spec, x=X, y=Y, colorscale='Viridis', showscale=False)
+            data=[trace]
+            layout = go.Layout( title="Spectrogram", width=800, height=400) 
+            fig = go.Figure(data=data, layout=layout)
+            st.plotly_chart(fig)
+            
+            tensor = tf.convert_to_tensor(spectrogram)
+            tensor = tf.expand_dims(tensor, 0)
+            tensor = np.array(tensor.numpy())
+            
+            prediction = MODEL.predict(tensor)
+            
+            # Bar chart of the prediction for each class
+            fig = go.Figure(data=[go.Bar(x=TARGETS, y=tf.nn.softmax(prediction[0]))])
+            fig.update_layout(title_text='Prediction for each class', width=800, height=400)
+            st.plotly_chart(fig)
+            st.markdown('##')
+    else:
+        st.markdown('##')
+        st.markdown('##')
+    
+with container_live:
+    st.write("Try the model with your own audio file")
+    wav_audio_data = st_audiorec() # tadaaaa! yes, that's it! :D
+
+    # add some spacing and informative messages
+    col_info, col_space = st.columns([0.57, 0.43])
+    with col_info:
+        st.write('\n')  # add vertical spacer
+        st.write('\n')  # add vertical spacer
+        st.write('The .wav audio data, as received in the backend Python code,'
+                 ' will be displayed below this message as soon as it has'
+                 ' been processed. [This informative message is not part of'
+                 ' the audio recorder and can be removed easily] 🎈')
+
+    if wav_audio_data is not None:
+        # display audio data as received on the Python side
+        col_playback, col_space = st.columns([0.58,0.42])
+        with col_playback:
+            st.audio(wav_audio_data, format='audio/wav')
