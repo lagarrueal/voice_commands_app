3ed3b4be32a920e315ede27935d03e8a2db9bcf3
Alexandre <lagarrueal@cy-tech.fr>
2023-02-02 15:02:20 +0100

    Debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,11 +143,10 @@ with container_live:
             audio = tf.io.decode_raw(wav_audio_data, tf.float32)
             st.write(type(audio))
             st.write(audio)
-            st.write(audio.numpy())
         except :
             st.write("nothing was done")
 
-        # audio = audio.numpy()
-        # audio = librosa.resample(audio, 44100, 16000)
+        audio = audio.numpy()
+        audio = librosa.resample(audio, 44100, 16000)
         # decoded = tf.audio.decode_wav(wav_audio_data, desired_channels=1, desired_samples=16000)
         # audio = decode_audio(wav_audio_data)
