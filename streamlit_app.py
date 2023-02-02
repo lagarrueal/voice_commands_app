4f4214a9540fb3018a41aaab2191c5ffb6d50902
Alexandre <lagarrueal@cy-tech.fr>
2023-02-02 14:56:16 +0100

    Debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,9 +139,13 @@ with container_live:
     if wav_audio_data is not None:
         st.write(type(wav_audio_data))
         # using librosa, resample to 16kHz
-        audio = tf.io.decode_raw(wav_audio_data, tf.float32)
-        print(type(audio))
-        audio = np.ndarray(shape=(len(audio),), dtype=np.float32, buffer=audio)
-        audio = librosa.resample(audio, 44100, 16000)
-        decoded = tf.audio.decode_wav(wav_audio_data, desired_channels=1, desired_samples=16000)
-        audio = decode_audio(wav_audio_data)
+        try :
+            audio = tf.io.decode_raw(wav_audio_data, tf.float32)
+            st.write(type(audio))
+        except :
+            st.write("nothing was done")
+
+        # audio = np.ndarray(shape=(len(audio),), dtype=np.float32, buffer=audio)
+        # audio = librosa.resample(audio, 44100, 16000)
+        # decoded = tf.audio.decode_wav(wav_audio_data, desired_channels=1, desired_samples=16000)
+        # audio = decode_audio(wav_audio_data)
