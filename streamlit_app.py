87f9108b3aaba07dfd9d84b8ab6115149fb2c1f4
Alexandre <lagarrueal@cy-tech.fr>
2023-02-02 15:01:08 +0100

    Debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,6 +143,7 @@ with container_live:
             audio = tf.io.decode_raw(wav_audio_data, tf.float32)
             st.write(type(audio))
             st.write(audio)
+            st.write(audio.numpy())
         except :
             st.write("nothing was done")
 
